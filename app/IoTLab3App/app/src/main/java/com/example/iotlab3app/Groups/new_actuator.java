fc985314e2ebf435562920721f5a1d8ad2600825
Nilsjeo <101942762+Nilsjeo123@users.noreply.github.com>
2023-01-07 21:26:51 +0100

    add async

--- app/IoTLab3App/app/src/main/java/com/example/iotlab3app/Groups/new_actuator.java
+++ app/IoTLab3App/app/src/main/java/com/example/iotlab3app/Groups/new_actuator.java
@@ -2,13 +2,18 @@ package com.example.iotlab3app.Groups;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.iotlab3app.Connection.ConnectionClass;
 import com.example.iotlab3app.Connection.SQLstuff;
+import com.example.iotlab3app.Login.LoginActivity;
 import com.example.iotlab3app.R;
 
 import java.sql.ResultSet;
@@ -38,25 +43,51 @@ public class new_actuator extends AppCompatActivity {
         });
 
         addAct.setOnClickListener(v -> {
+            new add_new_actuator().execute();
+        });
+
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    public class add_new_actuator extends AsyncTask<String, String, String> {
+        String z = null;
+        Boolean isSuccess = false;
+
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
             try {
                 if(nickname.getText().length() > 0 && location.getText().length() > 0 && ID.getText().length() > 0 && type.getText().length() > 0){
                     ResultSet rs = SQLstuff.runSQL("CALL NewActuator(" + SQLstuff.getHouse() +
                             ", '" + nickname.getText() + "', '" + ID.getText() +
                             "', '" + location.getText() + "', '" + type.getText() + "');");
 
-                    Toast.makeText(this, "New actuator added", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(new_actuator.this, "New actuator added", Toast.LENGTH_SHORT).show();
 
-                    Intent intent = new Intent(this, ActuatorList.class);
+                    Intent intent = new Intent(new_actuator.this, ActuatorList.class);
                     startActivity(intent);
 
                 } else {
-                    Toast.makeText(this, "Please insert text in all fields", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(new_actuator.this, "Please insert text in all fields", Toast.LENGTH_SHORT).show();
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        });
 
+
+            return z;
+        }
     }
 
 
